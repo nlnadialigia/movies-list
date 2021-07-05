520949c80d2ddfba95d46f2635725a078e56e561
Nadia <nlnadialigia@gmail.com>
2021-07-04 23:39:46 -0300

    refactor: fix duplicate module on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-
-import { MaterialModule } from './shared/material/material.module';
-import { TopoComponent } from './shared/components/topo/topo.component';
 import { RodapeComponent } from './shared/components/rodape/rodape.component';
-
-import { FilmesModule } from './filmes/filmes.module';
-
+import { TopoComponent } from './shared/components/topo/topo.component';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
@@ -26,7 +21,6 @@ import { FilmesModule } from './filmes/filmes.module';
     LayoutModule,
     MaterialModule,
     AppRoutingModule,
-    FilmesModule
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
